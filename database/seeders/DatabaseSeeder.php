05150412d52fa223118fc777be47f3247aacdd02
Howida-Ayman <HowidaAymanmohammed_sd23@fcis.bsu.edu.eg>
2026-02-21 16:53:57 +0200

    Refactor database seeding: add AcademicYearSeeder and remove DepartmentSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RolesSeeder::class,
             UsersSeeder::class,
-            DepartmentsSeeder::class
+            DepartmentsSeeder::class,
+            AcademicYearSeeder::class,
         ]
         );
 
